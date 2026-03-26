112b2e5c488d3e92af369a634dca979dda74238c
Lucadavih <101428517+Lucadavih@users.noreply.github.com>
2026-03-26 13:47:32 -0300

    Refactor load_configs function to accept a path

--- 5. Pratica/ProjetoDE/src/core.py
+++ 5. Pratica/ProjetoDE/src/core.py
@@ -1,15 +1,15 @@
 import yaml
 
 
-def load_configs():
+def load_configs(path: str = "assets/config.yml") -> dict:
     """
     Carrega arquivo de configuração YAML.
     """
-    with open("assets/config.yml", "r") as file:
+    with open(path, "r", encoding="utf-8") as file:
         configs = yaml.safe_load(file)
 
     return configs
 
 
-# variável esperada pelo app.py
+# variável usada pelo app.py
 configs = load_configs()
